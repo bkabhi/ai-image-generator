8cd9ddbb5792e936024b08146f984c3b569cf57a
bkabhi <bkabhijeet9801@gmail.com>
2023-01-20 20:06:08 +0530

    navbar with routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,22 @@ import { Home, CreatePost } from './pages'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link to={'/'}>
+          <img src={logo} alt="logo" className='w-28 object-contain'/>
+        </Link>
+        <Link to={'/create-post'} className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
+          Create
+        </Link>
+      </header>
+      <main className='sm:p-8 py-8 px-4 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/create-post' element={<CreatePost/>}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
