4b53d50b4558d73298e44ac348cf68bbc4940eca
bkabhi <bkabhijeet9801@gmail.com>
2023-01-20 22:13:46 +0530

    utils folder added for ramdom

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils'
+import { FormField, Loader } from '../components';
 const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
+    return (
+        <div>CreatePost</div>
+    )
 }
 
 export default CreatePost
---
+++ frontend/src/utils/index.js
@@ -0,0 +1,8 @@
+import { surpriseMePrompts } from "../constant";
+
+export const getRandomPrompt = (prompt)=>{
+    const randomIndex = Math.floor(Math.random()*surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex];
+    if(randomPrompt===prompt)return getRandomPrompt(prompt);
+    return randomPrompt;
+}
