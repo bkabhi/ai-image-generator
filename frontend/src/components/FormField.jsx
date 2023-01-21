d0a414bce3deda1ed55e8b2f15d9af4cebe6ef2f
bkabhi <bkabhijeet9801@gmail.com>
2023-01-21 09:10:35 +0530

    create post page added

--- frontend/src/components/FormField.jsx
+++ frontend/src/components/FormField.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 
-const FormField = () => {
-  return (
-    <div>FormField</div>
-  )
+const FormField = ({ labelName, type, name, value, placeholder, handleChange, isSurpriseMe, handleSurprise }) => {
+    return (
+        <div>
+            <div className='flex items-center gap-2 mb-2'>
+                <label
+                    htmlFor={name}
+                    className='block text-sm font-medium text-gray-900'>
+                    {labelName}
+                </label>
+                {isSurpriseMe && (
+                    <button
+                        type='button'
+                        onClick={handleSurprise}
+                        className='font-semibold text-xs bg-[#ececf1] py-1 px-2 rounded-[5px] text-black'
+                    >
+                        Surprise Me
+                    </button>
+                )}
+            </div>
+            <input
+                type={type}
+                id={name}
+                name={name}
+                placeholder={placeholder}
+                value={value}
+                onChange={handleChange}
+                required
+                className='bg-gray-50 border
+                border-gray-300 text-gray-900 text-sm 
+                rounded-lg focus:ring-[#4649ff]
+                focus:border-[#4649ff] outline-none block w-full p-3'
+            />
+        </div>
+    )
 }
 
 export default FormField
--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -4,9 +4,97 @@ import { useNavigate } from 'react-router-dom';
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils'
 import { FormField, Loader } from '../components';
+
 const CreatePost = () => {
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        name:'',
+        prompt:'',
+        photo:''
+    })
+    const [generatingImg, setgeneratingImg] = useState(false);
+    const [isLoading, setLoading] = useState(false);
+
+    const generateImage = () => {}
+
+    const handleSubmit = () => {
+
+    }
+
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name]:e.target.value})
+    }
+
+    const handleSurprise = () => {
+        const randomPrompt = getRandomPrompt(form.prompt)
+        setForm({...form, prompt:randomPrompt})
+    }
+
     return (
-        <div>CreatePost</div>
+        <section className='max-w-7xl mx-auto'>
+            <div>
+                <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+                <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>
+                    Generate an imaginative image through DALL-E AI and share it with the community
+                </p>
+            </div>
+            <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+                <div className='flex flex-col gap-5'>
+                    <FormField
+                        labelName='Your Name'
+                        type='text'
+                        name='name'
+                        placeholder='John Doe'
+                        handleChange = {handleChange}
+                    />
+                    <FormField
+                        labelName='prompt'
+                        type='text'
+                        name='prompt'
+                        placeholder='The long-lost Star Wars 1990 Japanese Anime'
+                        value={form.prompt}
+                        handleChange = {handleChange}
+                        isSurpriseMe
+                        handleSurprise={handleSurprise}
+                    />
+                    <div
+                        className='relative bg-gray-50 border
+                        border-gray-300 text-gray-900 text-sm rounded-lg
+                        focus:ring-blue-500 focus:border-blue-500 w-64 p-3
+                        h-64 flex justify-center items-center'
+                        >
+                            {form.photo?(
+                                <img src={form.photo} alt={form.prompt} className='w-full h-full object-contain'/>
+                            ) : (
+                                <img src={preview} alt='preview' className='w-9/12 h-9/12 object-contain opacity-40'/>
+                            )}
+                            {generatingImg && (
+                                <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                                    <Loader />
+                                </div>
+                            )}
+                    </div>
+                </div>
+                <div className='mt-5 flex gap-5'>
+                    <button 
+                        type='button'
+                        onClick = {generateImage}
+                        className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
+                            {generatingImg ?"Generating...": "Generate"}
+                    </button>
+                </div>
+                <div className='mt-10'>
+                    <p className='mt-2 text-[#666e75] text-[14px]'>
+                        ** Once you have created the image you want, you can share it with others in the community **
+                    </p>
+                    <button 
+                        type='submit'
+                        className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
+                            {isLoading ?"Sharing...": "Share With the community"}
+                    </button>
+                </div>
+            </form>
+        </section>
     )
 }
 
